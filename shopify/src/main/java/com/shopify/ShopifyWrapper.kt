9f868c16b107ff281610b00cef55dea56dc09c67
denysm <sleshvx@gmail.com>
2017-11-15 12:30:29 +0200

    Added separate component

--- shopify/src/main/java/com/shopify/ShopifyWrapper.kt
+++ shopify/src/main/java/com/shopify/ShopifyWrapper.kt
@@ -2,19 +2,33 @@ package com.shopify
 
 import android.content.Context
 import com.domain.ShopModule
+import com.domain.database.Dao
 import com.shopify.api.ShopifyApi
+import com.shopify.di.CheckoutModule
+import com.shopify.di.DaggerShopComponent
 import com.shopify.di.ShopComponent
 import com.shopify.router.ShopifyRouter
 
-class ShopifyModule(context: Context) : ShopModule {
+class ShopifyModule(context: Context, dao: Dao) : ShopModule {
 
     companion object {
         private const val BASE_URL = "lalkastore.myshopify.com"
         private const val ACCESS_TOKEN = "677af790376ae84213f7ea1ed56f11ca"
 
-        lateinit var shopComponent: ShopComponent
+        lateinit var component: ShopComponent
     }
 
     override val api = ShopifyApi(context, BASE_URL, ACCESS_TOKEN)
     override val router = ShopifyRouter()
+
+    init {
+        component = DaggerShopComponent.builder().checkoutModule(CheckoutModule(api, dao)).build()
+    }
+
+    /*var shopComponent: ShopComponent? = null
+        set(value) {
+            value?.let { component = it }
+        }*/
+
+
 }
---
+++ shopify/src/main/java/com/shopify/di/CheckoutModule.kt
@@ -0,0 +1,19 @@
+package com.shopify.di
+
+import com.domain.database.Dao
+import com.domain.network.Api
+import com.repository.CartRepository
+import com.repository.impl.CartRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class TestModule(private val api: Api, private val dao: Dao) {
+
+    @Provides
+    @Singleton
+    fun provideCartRepository(): CartRepository {
+        return CartRepositoryImpl(dao)
+    }
+}
