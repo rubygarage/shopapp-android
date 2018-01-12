afc39015145ce0865e1fc5495a284a1e76e6c016
denysm <sleshvx@gmail.com>
2018-01-12 14:28:31 +0200

    Remove commented code

--- domain/src/test/java/com/domain/detector/CardTypeDetectorTest.kt
+++ domain/src/test/java/com/domain/detector/CardTypeDetectorTest.kt
@@ -75,31 +75,5 @@ class CardTypeDetectorTest {
         Assert.assertNull(cardTypeDetector.detect("5019717010103742", context))
         Assert.assertNull(cardTypeDetector.detect("6331101999990016", context))
     }
-
-    /*@Test
-    fun testDetection() {
-
-
-        assertEquals(CardType.VISA, cardTypeDetector.detect("4000056655665556"))
-        assertEquals(CardType.VISA, cardTypeDetector.detect("4242424242424242"))
-
-        assertEquals(CardType.MASTERCARD, cardTypeDetector.detect("5105105105105100"))
-        assertEquals(CardType.MASTERCARD, cardTypeDetector.detect("5200828282828210"))
-        assertEquals(CardType.MASTERCARD, cardTypeDetector.detect("5555555555554444"))
-
-        assertEquals(CardType.AMERICAN_EXPRESS, cardTypeDetector.detect("371449635398431"))
-        assertEquals(CardType.AMERICAN_EXPRESS, cardTypeDetector.detect("378282246310005"))
-
-        assertEquals(CardType.DISCOVER, cardTypeDetector.detect("6011000990139424"))
-        assertEquals(CardType.DISCOVER, cardTypeDetector.detect("6011111111111117"))
-
-        assertEquals(CardType.DINERS_CLUB, cardTypeDetector.detect("30569309025904"))
-        assertEquals(CardType.DINERS_CLUB, cardTypeDetector.detect("38520000023237"))
-
-        assertEquals(CardType.JCB, cardTypeDetector.detect("3530111333300000"))
-        assertEquals(CardType.JCB, cardTypeDetector.detect("3566002020360505"))
-
-        assertEquals(CardType.UNKNOWN, cardTypeDetector.detect("0000000000000000"))
-    }*/
-
+    
 }
