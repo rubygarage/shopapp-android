dc43f8e7285cc30dd5947c41f36cd31cbbcd1e10
starikovskii <starikovskii.valentine@gmail.com>
2018-02-26 14:23:33 +0200

    finish blog module teste

---
+++ app/src/test/java/com/shopapp/ui/blog/BlogFragmentTest.kt
@@ -0,0 +1,125 @@
+package com.shopapp.ui.blog
+
+import android.content.Context
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.OrientationHelper
+import android.view.View
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
+import com.shopapp.TestShopApplication
+import com.shopapp.gateway.entity.Article
+import com.shopapp.ui.base.ui.FragmentVisibilityListener
+import com.shopapp.ui.blog.adapter.BlogAdapter
+import com.shopapp.ui.const.Constant
+import com.shopapp.ui.home.HomeActivity
+import com.shopapp.util.MockInstantiator
+import kotlinx.android.synthetic.main.fragment_blog.*
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment
+
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest = Config.NONE, application = TestShopApplication::class)
+class BlogFragmentTest {
+
+    private lateinit var fragment: BlogFragment
+    private lateinit var context: Context
+
+    companion object {
+        private const val DECORATOR_COUNT = 1
+    }
+
+    @Before
+    fun setUp() {
+        fragment = BlogFragment()
+        startFragment(fragment, HomeActivity::class.java)
+        context = RuntimeEnvironment.application.baseContext
+    }
+
+    @Test
+    fun shouldAddDecoratorOnCreate() {
+        assertEquals(DECORATOR_COUNT, fragment.recyclerView.itemDecorationCount)
+    }
+
+    @Test
+    fun shouldAddLinearLayoutManagerOnCreate() {
+        assertNotNull(fragment.recyclerView.layoutManager)
+        assertTrue(fragment.recyclerView.layoutManager is LinearLayoutManager)
+        val manager = fragment.recyclerView.layoutManager as LinearLayoutManager
+        assertEquals(OrientationHelper.VERTICAL, manager.orientation)
+    }
+
+    @Test
+    fun shouldAddAdapterOnCreate() {
+        assertNotNull(fragment.recyclerView.adapter)
+        assertTrue(fragment.recyclerView.adapter is BlogAdapter)
+        val adapter = fragment.recyclerView.adapter as BlogAdapter
+        assertEquals(0, adapter.itemCount)
+    }
+
+    @Test
+    fun shouldHideSeeAllOnCreate() {
+        assertEquals(View.GONE, fragment.seeAll.visibility)
+    }
+
+    @Test
+    fun shouldGetArticlesOnCreate() {
+        verify(fragment.presenter).loadArticles(Constant.DEFAULT_PER_PAGE_COUNT, null)
+    }
+
+    @Test
+    fun shouldLoadArticlesOnLoadDate() {
+        fragment.loadData()
+        verify(fragment.presenter, times(2)).loadArticles(Constant.DEFAULT_PER_PAGE_COUNT)
+    }
+
+    @Test
+    fun shouldNotifyVisibilityListener() {
+        val listener: FragmentVisibilityListener = mock()
+        fragment.visibilityListener = listener
+
+        val size = 5
+        val articleList = MockInstantiator.newList(MockInstantiator.newArticle(), size)
+        fragment.showContent(articleList)
+        verify(listener).changeVisibility(true)
+
+        fragment.showContent(emptyList())
+        verify(listener).changeVisibility(false)
+    }
+
+    @Test
+    fun shouldShowContentView() {
+        val articlesCount = 5
+        val articles = MockInstantiator.newList(MockInstantiator.newArticle(), articlesCount)
+        fragment.showContent(articles)
+        assertEquals(articlesCount, fragment.recyclerView.adapter.itemCount)
+    }
+
+    @Test
+    fun shouldNotShowContentView() {
+        val articlesCount = 0
+        fragment.showContent(emptyList())
+        assertEquals(articlesCount, fragment.recyclerView.adapter.itemCount)
+    }
+
+    @Test
+    fun shouldShowSeeAllView() {
+        val articlesCount = Constant.DEFAULT_PER_PAGE_COUNT
+        val articles = MockInstantiator.newList(MockInstantiator.newArticle(), articlesCount)
+        fragment.showContent(articles)
+        assertEquals(View.VISIBLE, fragment.seeAll.visibility)
+    }
+
+    @Test
+    fun shouldHideSeeAllView() {
+        fragment.showContent(emptyList())
+        assertEquals(View.GONE, fragment.seeAll.visibility)
+    }
+}
---
+++ domain/src/test/java/com/shopapp/domain/interactor/blog/ArticleListUseCaseTest.kt
@@ -0,0 +1,33 @@
+package com.shopapp.domain.interactor.blog
+
+import com.nhaarman.mockito_kotlin.verify
+import com.shopapp.domain.repository.BlogRepository
+import com.shopapp.gateway.entity.SortType
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.robolectric.annotation.Config
+
+@RunWith(MockitoJUnitRunner::class)
+@Config(manifest = Config.NONE)
+class ArticleListUseCaseTest {
+
+    private lateinit var useCase: ArticleListUseCase
+
+    @Mock
+    private lateinit var blogRepository: BlogRepository
+
+    @Before
+    fun setUpTest() {
+        useCase = ArticleListUseCase(blogRepository)
+    }
+
+    @Test
+    fun shouldDelegateCallToRepository() {
+        useCase.buildUseCaseSingle(ArticleListUseCase.Params(1, null))
+        verify(blogRepository).getArticleList(1, null, SortType.RECENT, true)
+    }
+
+}
