7f034e4da9ad4675d424e9d8a74f8d658fa8d7d2
starikovskii <starikovskii.valentine@gmail.com>
2018-03-01 14:26:43 +0200

    delete robolectric from preseter test

--- app/src/test/java/com/shopapp/ui/category/contract/CategoryPresenterTest.kt
+++ app/src/test/java/com/shopapp/ui/category/contract/CategoryPresenterTest.kt
@@ -16,11 +16,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
-@RunWith(RobolectricTestRunner::class)
-@Config(manifest = Config.NONE)
 class CategoryPresenterTest {
 
     companion object {
