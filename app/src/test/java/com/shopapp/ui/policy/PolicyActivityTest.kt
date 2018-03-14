4890207963af2f91a98442ddfcc5df5cba869d74
denysm <sleshvx@gmail.com>
2018-03-14 12:41:01 +0200

    Improve policy module tests

--- app/src/test/java/com/shopapp/ui/policy/PolicyActivityTest.kt
+++ app/src/test/java/com/shopapp/ui/policy/PolicyActivityTest.kt
@@ -6,7 +6,7 @@ import com.shopapp.test.MockInstantiator
 import com.shopapp.test.RxImmediateSchedulerRule
 import kotlinx.android.synthetic.main.activity_policy.*
 import kotlinx.android.synthetic.main.view_base_toolbar.view.*
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -15,6 +15,7 @@ import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
+import org.robolectric.fakes.RoboMenuItem
 
 @RunWith(RobolectricTestRunner::class)
 @Config(manifest = Config.NONE, application = TestShopApplication::class)
@@ -43,5 +44,21 @@ class PolicyActivityTest {
     @Test
     fun shouldSetPolicyBodyOnCreate() {
         assertEquals(MockInstantiator.DEFAULT_DESCRIPTION, activity.body.text.toString())
+    }
+
+    @Test
+    fun shouldFinishActivityWhenBackButtonClicked() {
+        val menuItem = RoboMenuItem(android.R.id.home)
+        activity.onOptionsItemSelected(menuItem)
+
+        assertTrue(activity.isFinishing)
+    }
+
+    @Test
+    fun shouldDoNothingWhenAnotherMenuItemClicked() {
+        val menuItem = RoboMenuItem(android.R.id.edit)
+        activity.onOptionsItemSelected(menuItem)
+
+        assertFalse(activity.isFinishing)
     }
 }
