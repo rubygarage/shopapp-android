afd26b18fcf3ca64db0a7c5ef282f008b90d914a
denysm <sleshvx@gmail.com>
2018-06-08 13:25:22 +0300

    Fix tests

--- app/src/test/java/com/shopapp/TestShopApplication.kt
+++ app/src/test/java/com/shopapp/TestShopApplication.kt
@@ -5,7 +5,6 @@ import com.shopapp.di.component.AppComponent
 import com.shopapp.di.component.DaggerTestAppComponent
 import com.shopapp.di.component.TestAppComponent
 import com.shopapp.gateway.Api
-import com.shopapp.gateway.entity.Config
 
 class TestShopApplication : ShopApplication() {
 
@@ -13,7 +12,7 @@ class TestShopApplication : ShopApplication() {
         lateinit var testAppComponent: TestAppComponent
     }
 
-    override fun buildAppComponent(api: Api?, dao: Dao?, config: Config?): AppComponent {
+    override fun buildAppComponent(api: Api?, dao: Dao?): AppComponent {
         testAppComponent = DaggerTestAppComponent.builder().build()
         return testAppComponent
     }
