6338978fe69b3a384e9facabd258fa5f19e40b2b
denysm <sleshvx@gmail.com>
2018-06-11 16:40:12 +0300

    Add @SuppressLint for @Parcelize

--- app/src/test/java/com/shopapp/ui/category/adapter/CategoryListAdapterTest.kt
+++ app/src/test/java/com/shopapp/ui/category/adapter/CategoryListAdapterTest.kt
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
